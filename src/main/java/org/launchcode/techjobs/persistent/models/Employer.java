40d1d93fd095fbbc73329bf9fe4d758d8803f5a1
adamstanislawsky <adamstanislawsky@gmail.com>
2024-11-16 00:20:53 -0600

    added location with needed parameters to employer model

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class Employer extends AbstractEntity {
+
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 150)
+    private String location;
+
+    public @NotNull @NotBlank @Size(min = 3, max = 150) String getLocation() {
+        return location;
+    }
+
+    public void setLocation(@NotNull @NotBlank @Size(min = 3, max = 150) String location) {
+        this.location = location;
+    }
+
 }
