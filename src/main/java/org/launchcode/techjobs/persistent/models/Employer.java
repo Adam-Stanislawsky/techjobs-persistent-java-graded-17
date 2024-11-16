5dacfcd4b4c480dc0bba05eecbe8d461151ba7a3
adamstanislawsky <adamstanislawsky@gmail.com>
2024-11-16 00:32:24 -0600

    added entity and annotations for emp & skill models

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,15 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Employer extends AbstractEntity {
+
 
     @NotNull
     @NotBlank
     @Size(min = 3, max = 150)
     private String location;
+
+//    may need the regular constructor here
+//    public Employer(String location){
+//        this.location = location;
+//    }
+
+    public Employer(){}
 
     public @NotNull @NotBlank @Size(min = 3, max = 150) String getLocation() {
         return location;
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 500)
+    public String description;
+
+    //also check here if the regular constructor is needed
+    public Skill(){}
+
+    public @NotNull @NotBlank @Size(min = 3, max = 500) String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@NotNull @NotBlank @Size(min = 3, max = 500) String description) {
+        this.description = description;
+    }
 }
